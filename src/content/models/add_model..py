33af9f69fef4c2516583fb0447119c3b9769f092
David Starr <dastarr@microsoft.com>
2024-02-04 12:17:54 -0800

    Delete unused models

--- src/content/models/add_model..py
+++
@@ -1,7 +0,0 @@
-class AddModel():
-    
-    def __init__(self, playlists, content):
-        self.playlists = playlists
-        self.content = content
-        
-    
--- src/content/models/edit_model.py
+++
@@ -1,7 +0,0 @@
-class EditModel():
-    
-    def __init__(self, playlists, content):
-        self.playlists = playlists
-        self.content = content
-        
-    
